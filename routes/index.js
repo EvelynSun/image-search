74731c6eda89b1fc7644967f846c676ecb2c020c
Evelyn <evelyn.sun1@gmail.com>
2016-07-31 18:13:44 +0000

    modfied

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@
 module.exports = function(app) {
     app.get('/',function(req,res,next){
         res.render('index', {
-            title:"BaseJump:Image Search Abstraction Layer"
-            
+            title:"BaseJump:Image Search Abstraction Layer",
+            search_url:"https://evelyn-image-search1.herokuapp.com/api/imagesearch/lolcats%20funny?offset=10",
+            recent_url:"https://evelyn-image-search1.herokuapp.com/api/imagesearch/latest"
         });
     })
 }
